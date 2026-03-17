4c982169c189c84497d165b97fd993b6a2b982e9
Ame1ia4 <ameliamolon@gmail.com>
2026-03-17 16:27:59 +0000

    Blink.py fix

--- comms/blink.py
+++ comms/blink.py
@@ -1,37 +1,12 @@
-"""
-This Raspberry Pi code was developed by newbiely.com
-This Raspberry Pi code is made available for public use w>
-For comprehensive instructions and wiring diagrams, pleas>
-https://newbiely.com/tutorials/raspberry-pi/raspberry-pi->
-"""
-
 import RPi.GPIO as GPIO
 import time
 
-# Set the GPIO mode and warning
+LED_PIN = 18
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+GPIO.setup(LED_PIN, GPIO.OUT)
 
-# Define the GPIO18 pin number
-led_pin = 18
-
-# Setup the GPIO pin as an output
-GPIO.setup(led_pin, GPIO.OUT)
-
-# Function to blink the LED
-def blink_led():
-    GPIO.output(led_pin, GPIO.HIGH)
-    time.sleep(1)  # LED on for 1 second
-    GPIO.output(led_pin, GPIO.LOW)
-    time.sleep(1)  # LED off for 1 second
-
-try:
-    # Run the LED blinking forever until a keyboard interrupt (Ctrl + C)
-    while True:
-        blink_led()
-
-except KeyboardInterrupt:
-    # Cleanup GPIO settings on keyboard interrupt
-    GPIO.cleanup()
-
+def green_on(duration=20):
+    GPIO.output(LED_PIN, GPIO.HIGH)
+    time.sleep(duration)
+    GPIO.output(LED_PIN, GPIO.LOW)
